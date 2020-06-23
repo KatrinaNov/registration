56f825932a120ca1515230f5fd9a35ebf9d906b7
katrina <katrina.nov13@gmail.com>
2020-06-23 19:01:46 +0300

    add registration

--- script.js
+++ script.js
@@ -5,10 +5,8 @@ const userNameBlock = document.querySelector('.name'),
       logInBtn = document.querySelector('.logIn'),
       userList = document.querySelector('.userList');
 
-let date = new Date();
 
 let formatter = new Intl.DateTimeFormat("ru", {
-  weekday: "long",
   year: "numeric",
   month: "long",
   day: "numeric",
@@ -17,34 +15,68 @@ let formatter = new Intl.DateTimeFormat("ru", {
   second: "numeric"
 });
 
-const render = function(userName, userSurname) {
-  let userItem = document.createElement('li');
-  userItem.innerHTML = `${userName}, ${userSurname}, ${formatter.format(date)}`;
-  userList.append(userItem);
+
+const render = function(item) {
+    let userItem = document.createElement('li');
+    userItem.innerHTML = `Имя: ${item.userFullName.split(' ')[0]}, фамилия: ${item.userFullName.split(' ')[1]}, зарегистрирован: ${item.date}`;
+    userList.append(userItem);  
 };
 
-let users =[] || JSON.parse(localStorage.getItem('users'));
+let users = JSON.parse(localStorage.getItem('users')) || [];
 
 signUpBtn.addEventListener('click', function(){
-  let a = prompt('Введите ваше имя и фамилию через пробел', 'Иван Иванов'),
-      b = prompt('Введите ваш логин'),
-      c = prompt('Введите пароль');
+  // ввод имени и фамилии  + проверка на пробелы
+  let a, b, c;
+  do {
+     a = prompt('Введите ваше имя и фамилию через пробел', 'Иван Иванов');
+     a = a.trim();
+     if (a.lastIndexOf(' ') !== a.indexOf(' ')) {
+       alert('Слишком много пробелов');
+     }
+     if (a.indexOf(' ') === -1) {
+       alert('Кажется вы забыли ввести фамилию');
+     }
+
+  } while (a === '' || (a.lastIndexOf(' ') !== a.indexOf(' ')) || a.indexOf(' ') === -1);
+
+  do {
+    b = prompt('Введите ваш логин');
+    if (b === '') {
+      alert('Логин обязателен к заполнению');
+    }
+  } while (b === '' || b === null);
+
+  do {
+    c = prompt('Введите пароль');
+    if (c === '') {
+      alert('Без пароля никак');
+    }
+  } while (c === '' || c === null);
+      
 
   let userData = {};
   userData.userFullName = a;
   userData.login = b;
   userData.password = c;
+  userData.date = formatter.format(new Date());
 
   users.push(userData);
   localStorage.setItem('users', JSON.stringify(users));
 
-  let userName = userData.userFullName.split(' ')[0],
-    userSurname = userData.userFullName.split(' ')[1];
-  render(userName, userSurname);
+
+    render(userData);
+
 
   console.log(users);
 
 });
+
+console.log(users);
+users.forEach(function(item) {
+  render(item);
+});
+
+
 
 
 
